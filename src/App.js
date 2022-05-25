f5af5d43c64fc6cf2d2aa186b40edad8ee80609d
ProgrammerMasum <masumjsbd7@gmail.com>
2022-05-25 14:03:32 +0600

    React core concept learned

--- src/App.js
+++ src/App.js
@@ -2,6 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person = {
+    name: "Masum Billah",
+    job:"Player"
+  }
+  var person2 = {
+    name: "Nishat",
+    job:"Dancer"
+  }
+  var person3 = {
+    name: "Dolon",
+    job:"Heroine"
+  }
+var style = {
+  color:'black',
+  backgroundColor: 'lightgray',
+  border: '3px solid green',
+  
+}
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +36,10 @@ function App() {
         >
           Learn React
         </a>
+        <h3 className='' style={style}>My heading: {person.name +" "+ person.job} </h3>
+        <h4 style={{backgroundColor: 'cyan', color:'white', border: '2px solid lightgray'}}>Dancer: {person2.name + " "+ person2.job}</h4>
+        <h5>Heroine: {person3.name + " "+ person3.job}</h5>
+        <p>My first React Paragraph</p>
       </header>
     </div>
   );
